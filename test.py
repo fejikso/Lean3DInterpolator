ec6c74c25a8b47de36714198a8412aa3db9a7bea
Fernando G. del Cueto <fejikso@users.noreply.github.com>
2023-11-15 11:59:13 -0700

    Update test.py
    Changed to better var names.

--- test.py
+++ test.py
@@ -8,10 +8,10 @@ if __name__ == "__main__":
     test_vol = np.random.rand(100, 100, 100)    
 
     # Initialize the LeanVolumeInterpolator
-    lint = LeanVolumeInterpolator(test_vol, extrap_val=np.nan, dtype=np.float32, to_dense=False)
+    lean_interp = LeanVolumeInterpolator(test_vol, extrap_val=np.nan, dtype=np.float32, to_dense=False)
 
     # Single coordinate interpolation
-    single_val = lint((51.5, 13.1, 10.5))
+    single_val = lean_interp((51.5, 13.1, 10.5))
     print(f"Interpolated value at a single coordinate (51.5, 13.1, 10.5): {single_val}")
 
     # Vector of coordinates interpolation
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     y_coords = np.linspace(0, 99, 50)  # 50 evenly spaced coordinates along y
     z_coords = np.linspace(0, 99, 50)  # 50 evenly spaced coordinates along z
 
-    vector_vals = lint((x_coords, y_coords, z_coords))
+    vector_vals = lean_interp((x_coords, y_coords, z_coords))
     print(f"Interpolated values at 50 vectorized coordinates: {vector_vals}")
 
     # Demonstrating efficiency with a larger set of coordinates
@@ -27,5 +27,5 @@ if __name__ == "__main__":
     large_y_coords = np.random.uniform(0, 99, 1000)
     large_z_coords = np.random.uniform(0, 99, 1000)
 
-    large_vector_vals = lint((large_x_coords, large_y_coords, large_z_coords))
+    large_vector_vals = lean_interp((large_x_coords, large_y_coords, large_z_coords))
     print(f"Interpolated values at 1000 random coordinates: {large_vector_vals}")
